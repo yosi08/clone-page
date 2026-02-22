f48311a3f6d8cfa466e96582712c3aad1e98d35c
yosi08 <stress0803@gmail.com>
2026-02-22 21:37:53 +0900

    correction

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,4 +7,5 @@ export default function Navbar() {
       </button>
     </nav>
   );
+  // 네브바 구축을 위한 컴포넌트입니다. Tailwind CSS를 사용하여 스타일링을 적용하였습니다. 네브바는 검은색 배경에 빨간색 텍스트로 "NETFLIX"라는 제목과 오른쪽에 로그인 버튼이 있습니다.
 }
